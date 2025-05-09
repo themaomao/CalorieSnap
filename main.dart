35714f0ad21714c74d55b6fc06434f9e6ec21d37
Maomao <117374532+themaomao@users.noreply.github.com>
2025-05-08 22:02:26 -0400

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(CalorieSnapApp());
+
+class CalorieSnapApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CalorieSnap',
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: Text('CalorieSnap')),
+        body: Center(child: Text('欢迎使用 CalorieSnap')),
+      ),
+    );
+  }
+}
